37f839e848b9c7b1af573b91ed59b536787f8c3a
mariano.fresno <mnofresno@gmail.com>
2021-06-17 23:58:56 -0300

    Add dockerfile, package and editorconfig for the api

--- api/app.js
+++ api/app.js
@@ -1,10 +1,17 @@
 // Load dependencies
 const express    = require('express'),
+      Influx     = require('influx'),
       bodyParser = require('body-parser'),
       winston    = require('winston');
 
 // Create express application
 let app = module.exports = express();
+
+// Create a client towards InfluxDB
+let influx = new Influx.InfluxDB({
+   host: process.env.INFLUXDB_HOST || "db",
+   database: process.env.INFLUXDB_DATABASE || "iot"
+});
 
 // Body parser configuration
 app.use(bodyParser.json());
@@ -12,7 +19,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handle incoming data
 app.post('/data',
-         (req, res, next) => {
-             winston.info(req.body);
-             return res.sendStatus(201);
-         });
+    function(req, res, next){
+        influx.writePoints([{
+            measurement: 'data',
+            tags: { type: req.body.type },
+            fields: { sensor_id: req.body.sensor_id, value: req.body.value },
+            timestamp: new Date(req.body.ts).getTime() * 1000000
+        }]).then(() => {
+            winston.info(req.body);
+            return res.sendStatus(201);
+        }).catch(err => {
+            winston.error(err.message);
+            return res.sendStatus(500);
+        }
+    );
+});
